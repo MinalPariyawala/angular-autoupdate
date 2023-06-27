1b761ad41cfc235d7cd266724abb52b00e519f6b
MinalPariyawala <pariyawalaminal96@gmail.com>
2023-06-27 19:17:54 +0530

    Add change in autoupdate method

--- desktop/main.ts
+++ desktop/main.ts
@@ -1,21 +1,39 @@
 
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, screen } from 'electron';
 import * as path from 'path';
 import { autoUpdater } from 'electron-updater';
 import * as log from "electron-log";
 
 log.transports.file.resolvePath = () => path.join('C:/Users/Administrator/Desktop/minal/angular-autoupdate/', 'logs/main.log');
-log.info('application version:', app.getVersion());
 log.info('hello');
+log.info('application version:', app.getVersion());
 
 export let win: any;
 
 function createWindow() {
+
+    const size = screen.getPrimaryDisplay().workAreaSize;
     win = new BrowserWindow({
-        width: 300, height: 400
+        x: 0,
+        y: 0,
+        width: size.width,
+        height: size.height,
+        webPreferences: {
+            nodeIntegration: true,
+        },
     });
     win.loadURL('http://localhost:4201');
     // win.loadFile(path.join(__dirname, 'src/index.html'));
+
+    // The following is optional and will open the DevTools:
+    // win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store window
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 }
 
 
@@ -24,8 +42,10 @@ app.on('ready', () => {
     autoUpdater.checkForUpdatesAndNotify();
 })
 
-autoUpdater.on('update-available', () => {
+autoUpdater.on('update-available', (info) => {
     log.info('update-available');
+    log.info('version', info.version);
+    log.info('release date', info.releaseDate);
 })
 
 autoUpdater.on('checking-for-update', () => {
@@ -33,7 +53,7 @@ autoUpdater.on('checking-for-update', () => {
 })
 
 autoUpdater.on('download-progress', (progress) => {
-    log.info('download-progress', progress);
+    log.info('download-progress', Math.floor(progress.percent));
 })
 
 autoUpdater.on('update-downloaded', () => {
